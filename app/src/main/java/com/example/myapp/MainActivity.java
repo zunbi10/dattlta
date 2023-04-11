a32a4884f61650d1945e2cbc10b887598ee7345f
zunbi10 <ofhuy710@gmail.com>
2023-04-11 13:07:23 +0700

    chinh sua layout + lam phan nhap/hien ten nguoi choi

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -6,21 +6,53 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 Button btnAI,btnBD;
+boolean isFirst = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnAI= findViewById(R.id.buttonAI);
+
+
+        EditText textName = findViewById(R.id.textName);
+        btnAI = findViewById(R.id.buttonAI);
+
+
         btnAI.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+                String name = textName.getText().toString().trim();
                 Intent intent = new Intent(MainActivity.this, QuestionAI1.class);
+                intent.putExtra(QuestionAI1.NAME, name);
                 startActivity(intent);
             }
         });
+
+
+        textName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textName.setText("");
+            }
+        });
+
+
+        textName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (isFirst) {
+                    textName.setText("");
+                    isFirst = false;
+                }
+            }
+        });
     }
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, QuestionAI1.class);
+                startActivity(intent);
+            };
 }
--- app/src/main/java/com/example/myapp/QuestionAI1.java
+++ app/src/main/java/com/example/myapp/QuestionAI1.java
@@ -6,44 +6,82 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class QuestionAI1 extends AppCompatActivity {
-Button btn_q1, btn_q2, btn_q3, btn_q4;
+    public static String NAME = "";
+    Button btn_q1, btn_q2, btn_q3, btn_q4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_ai1);
-        btn_q1= findViewById(R.id.button_AI_q1_a1);
-        btn_q2= findViewById(R.id.button_AI_q1_a2);
-        btn_q3= findViewById(R.id.button_AI_q1_a3);
-        btn_q4= findViewById(R.id.button_AI_q1_a4);
-                btn_q1.setOnClickListener(new View.OnClickListener() {
+
+        init();
+
+        TextView textView1 = findViewById(R.id.textView1);
+        TextView textView2 = findViewById(R.id.textView4);
+        Intent intent = getIntent();
+        textView1.setText("Chào " + intent.getStringExtra(NAME));
+
+        RadioGroup group = findViewById(R.id.radioGroup);
+        group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(QuestionAI1.this, QuestionAI2.class);
-                startActivity(intent);
-            }
-        });
-        btn_q2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(QuestionAI1.this, QuestionAI2.class);
-                startActivity(intent);
-            }
-        });
-        btn_q3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(QuestionAI1.this, QuestionAI2.class);
-                startActivity(intent);
-            }
-        });
-        btn_q4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(QuestionAI1.this, QuestionAI2.class);
-                startActivity(intent);
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedRadioId) {
+                switch (checkedRadioId){
+                    case R.id.button_AI_q1_a1:
+                        textView2.setText("Bạn chọn đáp án A");
+                        break;
+                    case R.id.button_AI_q1_a2:
+                        textView2.setText("Bạn chọn đáp án B");
+                        break;
+                    case R.id.button_AI_q1_a3:
+                        textView2.setText("Bạn chọn đáp án C");
+                        break;
+                    case R.id.button_AI_q1_a4:
+                        textView2.setText("Bạn chọn đáp án D");
+                        break;
+                }
             }
         });
     }
+    void init(){
+        btn_q1 = findViewById(R.id.button_AI_q1_a1);
+        btn_q2 = findViewById(R.id.button_AI_q1_a2);
+        btn_q3 = findViewById(R.id.button_AI_q1_a3);
+        btn_q4 = findViewById(R.id.button_AI_q1_a4);
+    }
+//        btn_q1= findViewById(R.id.button_AI_q1_a1);
+//        btn_q2= findViewById(R.id.button_AI_q1_a2);
+//        btn_q3= findViewById(R.id.button_AI_q1_a3);
+//        btn_q4= findViewById(R.id.button_AI_q1_a4);
+
+//        btn_q1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(QuestionAI1.this, QuestionAI2.class);
+//                startActivity(intent);
+//            }
+//        });
+//        btn_q2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(QuestionAI1.this, QuestionAI2.class);
+//                startActivity(intent);
+//            }
+//        });
+//        btn_q3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(QuestionAI1.this, QuestionAI2.class);
+//                startActivity(intent);
+//            }
+//        });
+//        btn_q4.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(QuestionAI1.this, QuestionAI2.class);
+//                startActivity(intent);
+//            }
+//        });
 }
